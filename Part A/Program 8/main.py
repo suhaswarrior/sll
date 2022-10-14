c40928c7d0bd2de4a576fce3feedbf8f78e9b7d8
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-10-14 19:58:00 +0530

    Add files via upload

---
+++ Part A/Program 8/main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from functools import reduce # for reduce function
+list = [] # list to store the numbers
+for i in range(0,6): # for loop to take input
+    item=int(input("Enter the number "))
+    list.append(item)
+
+print("\nThe elements are\n") # printing the elements
+print(list)
+print("Elements after multiplying are:\n")
+newlist = [x*3 for x in list] # list comprehension
+print(newlist)
+listSum = reduce(lambda a,b:a+b,list) # using reduce function
+print("The sum of the original list  is ",listSum) # printing the sum
+
+listSum = reduce(lambda a,b:a+b,newlist) # using reduce function
+print("The sum of the original list  is ",listSum) # printing the sum
