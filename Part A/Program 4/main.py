110b73e8b1130574cb5669234e79a8b4edc014af
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-11-25 18:23:04 +0530

    Update

--- Part A/Program 4/main.py
+++ Part A/Program 4/main.py
@@ -1,3 +1,4 @@
+
 print("Temperature Converter")
 print("1. Celsius to Fahrenheit")
 print("2. Fahrenheit to Celsius")
@@ -5,61 +6,86 @@ print("3. Celsius to Kelvin")
 print("4. Kelvin to Celsius")
 print("5. Fahrenheit to Kelvin")
 print("6. Kelvin to Fahrenheit")
-print("7. Exit")
+print("7. View History")
+print("8. Exit")
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
 
 
-def celsius_to_fahrenheit():
-    celsius = float(input("Enter temperature in Celsius: "))
-    fahrenheit = (celsius * 1.8) + 32
-    print("%.2f Celsius is: %.2f Fahrenheit" %(celsius, fahrenheit))
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
 
 
-def fahrenheit_to_celsius():
-    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
-    celsius = (fahrenheit - 32) / 1.8
-    print("%.2f Fahrenheit is: %.2f Celsius" %(fahrenheit, celsius))
+def celsius_to_kelvin(celsius):
+    return celsius + 273.15
 
 
-def celsius_to_kelvin():
-    celsius = float(input("Enter temperature in Celsius: "))
-    kelvin = celsius + 273.15
-    print("%.2f Celsius is: %.2f Kelvin" %(celsius, kelvin))
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
 
 
-def kelvin_to_celsius():
-    kelvin = float(input("Enter temperature in Kelvin: "))
-    celsius = kelvin - 273.15
-    print("%.2f Kelvin is: %.2f Celsius" %(kelvin, celsius))
+def fahrenheit_to_kelvin(fahrenheit):
+    return (fahrenheit + 459.67) * 5/9
 
 
-def fahrenheit_to_kelvin():
-    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
-    kelvin = (fahrenheit + 459.67) * 5/9
-    print("%.2f Fahrenheit is: %.2f Kelvin" %(fahrenheit, kelvin))
+def kelvin_to_fahrenheit(kelvin):
+    return kelvin * 9/5 - 459.67
 
 
-def kelvin_to_fahrenheit():
-    kelvin = float(input("Enter temperature in Kelvin: "))
-    fahrenheit = kelvin * 9/5 - 459.67
-    print("%.2f Kelvin is: %.2f Fahrenheit" %(kelvin, fahrenheit))
+def view_history(history):
+    print("1. Sort by From Unit")
+    print("2. Sort by To Unit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        history.sort(key=lambda x: x[0])
+    elif choice == 2:
+        history.sort(key=lambda x: x[1])
+    else:
+        print("Invalid Choice")
+    for i in history:
+        print(i)
 
 
-while 0==0:
-    print("Enter choice: ")
+history = []
+while True:
     choice = int(input("Enter your choice: "))
     if choice == 1:
-        celsius_to_fahrenheit()
+        celsius = float(input("Enter temperature in Celsius: "))
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        history.append((celsius, fahrenheit))
+        print("Temperature in Fahrenheit: ", fahrenheit)
     elif choice == 2:
-        fahrenheit_to_celsius()
+        fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+        celsius = fahrenheit_to_celsius(fahrenheit)
+        history.append((fahrenheit, celsius))
+        print("Temperature in Celsius: ", celsius)
     elif choice == 3:
-        celsius_to_kelvin()
+        celsius = float(input("Enter temperature in Celsius: "))
+        kelvin = celsius_to_kelvin(celsius)
+        history.append((celsius, kelvin))
+        print("Temperature in Kelvin: ", kelvin)
     elif choice == 4:
-        kelvin_to_celsius()
+        kelvin = float(input("Enter temperature in Kelvin: "))
+        celsius = kelvin_to_celsius(kelvin)
+        history.append((kelvin, celsius))
+        print("Temperature in Celsius: ", celsius)
     elif choice == 5:
-        fahrenheit_to_kelvin()
+        fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+        kelvin = fahrenheit_to_kelvin(fahrenheit)
+        history.append((fahrenheit, kelvin))
+        print("Temperature in Kelvin: ", kelvin)
     elif choice == 6:
-        kelvin_to_fahrenheit()
+        kelvin = float(input("Enter temperature in Kelvin: "))
+        fahrenheit = kelvin_to_fahrenheit(kelvin)
+        history.append((kelvin, fahrenheit))
+        print("Temperature in Fahrenheit: ", fahrenheit)
     elif choice == 7:
-        exit()
+        view_history(history)
+    elif choice == 8:
+        break
     else:
-        print("Invalid choice")
+        print("Invalid Choice")
+        
